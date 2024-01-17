9031b21591c1cfc70d3138d425644a2377321072
João Bosco Dos Reis Becker <joao.jbosco@gmail.com>
2024-01-17 17:39:32 -0300

    Identação de código

--- Program.cs
+++ Program.cs
@@ -7,10 +7,8 @@ using System.Reflection.Metadata;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 var app = builder.Build();
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 app.MapRazorPages();
-
 app.Run();
